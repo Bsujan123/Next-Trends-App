7b6088eb578b9198e552ef63eee4f4f29093d47c
Bsujan123 <142375651+Bsujan123@users.noreply.github.com>
2024-03-14 12:21:44 +0530

    Update index.js

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -10,8 +10,15 @@ class LoginForm extends Component {
     password: '',
     showSubmitError: false,
     errorMsg: '',
+    credit:false,
   }
-
+openDetails = () => {
+  this.setState(pre => ({credit:!pre.credit}))
+} 
+loginDetails = () => <div>
+<p>user-name: rahul</p>
+<p>password: rahul@2021</p> 
+</div>
   onChangeUsername = event => {
     this.setState({username: event.target.value})
   }
@@ -88,7 +95,7 @@ class LoginForm extends Component {
   }
 
   render() {
-    const {showSubmitError, errorMsg} = this.state
+    const {showSubmitError, errorMsg,credit} = this.state
     const jwtToken = Cookies.get('jwt_token')
     if (jwtToken !== undefined) {
       return <Redirect to="/" />
@@ -117,6 +124,12 @@ class LoginForm extends Component {
             Login
           </button>
           {showSubmitError && <p className="error-message">*{errorMsg}</p>}
+          <div>
+            <button type="button" className='login-button' onClick={this.openDetails}>CREDENTIALS</button>
+            {credit? this.loginDetails():'' }
+           
+            
+          </div>
         </form>
       </div>
     )
