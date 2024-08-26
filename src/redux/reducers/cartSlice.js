964746381cc958b9cc24117fb6ae94710660c178
Bsujan123m <bandarusujan961@gmail.com>
2024-08-26 12:17:19 +0530

    modified redux toolkit

--- src/redux/reducers/cartSlice.js
+++ src/redux/reducers/cartSlice.js
@@ -1,4 +1,4 @@
-import {createSlice,createAsyncThunk} from '@reduxjs/toolkit';
+import {createSlice} from '@reduxjs/toolkit';
 
 
 const state = {
@@ -8,16 +8,7 @@ const state = {
     error:false
 }
 
-export const productData = createAsyncThunk('user/cart',async(id)=>{
-    try{
-        const res = await fetch(`https://fakestoreapi.com/products/${id}`)
-        const data = await res.json()
-        return data
-    }catch(error){
-        throw error
-    }
-    
-})
+
 
 
 const cartSlice = createSlice({
@@ -33,20 +24,7 @@ const cartSlice = createSlice({
             state.balance = state.balance - action.payload.price
         }
     },
-    extraReducers:(builder)=>{
-        builder.addCase(productData.pending,(state)=>{
-            state.status = "loading"
-        })
-        .addCase(productData.fulfilled,(state,action)=>{
-            state.status = "completed"
-            state.products = action.payload
-        })
-        .addCase(productData.rejected,(state,action)=>{
-            state.status = "rejected"
-            state.products = []
-            state.error = action.error.message
-        })
-    },
+    
 })
 
 export const { addProduct ,removeProduct} = cartSlice.actions;
